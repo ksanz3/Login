187a1c9a04afc384afa610ba0ac5be99facd1b48
ksanz3 <Secrets003@gmail.com>
2022-04-22 11:53:51 -0400

    registro service y api

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { UsersService } from "../users.service";
 
 @Component({
   selector: "app-register",
@@ -9,11 +10,14 @@ export class RegisterComponent {
   email!: string;
   password!: string;
   confirmPassword!: string;
+  passwordError!: boolean;
 
-  constructor() {}
+  constructor(public userService: UsersService) {}
 
   register() {
-    console.log(this.email);
-    console.log(this.password);
+    const user = { email: this.email, password: this.password };
+    this.userService.register(user).subscribe(data => {
+      console.log(data);
+    });
   }
 }
