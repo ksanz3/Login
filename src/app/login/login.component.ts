85c444d421a022e43a7a0f26b21543f66d321bac
ksanz3 <Secrets003@gmail.com>
2022-04-25 13:21:19 -0400

    footer

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import { UsersService } from '../users.service';
 import { Router } from '@angular/router';
 
@@ -7,13 +7,23 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnChanges, OnDestroy {
   email!: string;
   password!: string;
 
+  ngOnDestroy(): void {
+    console.log('Destroy')
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Changes')
+  }
+  ngOnInit(): void {
+    console.log('Init')
+  }
+
   constructor(public userService: UsersService, public router: Router) { }
 
-  login() {
+   login() {
     const user = {email: this.email, password: this.password};
     this.userService.login(user).subscribe( data => {
       this.userService.setToken(data.token);
